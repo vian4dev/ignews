046daeeaca00a3f9d07a43dad561a47bb9fae3a4
Gabriel Viana <viannadoze@gmail.com>
2022-09-27 22:52:42 -0300

    login with github authorization

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import { AppProps } from 'next/app'
 import '../../styles/global.scss';
 import { Header } from './components/Header';
+import { SessionProvider as NextAuthProvider } from 'next-auth/react'
 
-function MyApp({ Component, pageProps }: AppProps) {
+
+function MyApp({ Component, pageProps }: AppProps<{ session }>) {
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   )
 }
 
